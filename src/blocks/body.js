4bd7b7425be274961a6cd33c84d4e0ef033359ea
aidfromdeagland <aidfromdeagland@gmail.com>
2020-01-10 12:43:26 +0300

    autotests struggling, round 3

--- src/blocks/body.js
+++ src/blocks/body.js
@@ -1,16 +1,14 @@
 import {switchSelector, switchCheckedSelector, changeColorTheme} from './onoffswitch';
 import {accordionVisibleSelector, accordionHidingSelector} from './e-accordion';
 
-
-document.addEventListener("DOMContentLoaded", 
-document.body.addEventListener('click', function() {
+window.document.body.addEventListener('click', function() {
   if (event.target.closest('.' + switchSelector)) {
     changeColorTheme();
     event.target.closest('.' + switchSelector).classList.toggle(switchCheckedSelector);
   } else if (event.target.closest('.' + accordionVisibleSelector)) {
     event.target.closest('.' + accordionVisibleSelector).nextSibling.classList.toggle(accordionHidingSelector);
   }
-}));
+});
 
 
 
